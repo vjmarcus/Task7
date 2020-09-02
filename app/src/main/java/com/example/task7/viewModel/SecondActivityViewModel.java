c1da1b5f9fecd312bd9556cc081910bcbb87a096
vjmarcus <vjmarcus@list.ru>
2020-09-02 10:42:26 +0300

    Ready to add ROOM

--- app/src/main/java/com/example/task7/viewModel/SecondActivityViewModel.java
+++ app/src/main/java/com/example/task7/viewModel/SecondActivityViewModel.java
@@ -5,10 +5,18 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.example.task7.repository.StoryRepository;
+
 // Может вызывать только методы модели, меняет свое состояние, своих полей
 public class SecondActivityViewModel extends AndroidViewModel {
 
+    private StoryRepository storyRepository;
+
     public SecondActivityViewModel(@NonNull Application application) {
         super(application);
+    }
+
+    public void setDataFromIntent() {
+
     }
 }
